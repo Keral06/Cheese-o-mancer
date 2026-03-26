0f7c4ec0001e00f34d0b220b5d68cbc6d1e283c0
mecMocmec <e.ibanez3426@gmail.com>
2026-03-26 15:42:25 +0100

    Added missing texture

---
+++ assets/maps/test2_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="test2" tilewidth="128" tileheight="128" tilecount="3" columns="3">
+ <image source="LV1_test_tileset.png" width="384" height="128"/>
+</tileset>
