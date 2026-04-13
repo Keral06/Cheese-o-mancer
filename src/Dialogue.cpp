fa157e1ad3ea6b3018de1be91d092a7da662f96f
irentren <icalatravasierra@gmail.com>
2026-04-13 12:13:04 +0200

    Added dialogue!!

---
+++ src/Dialogue.cpp
@@ -0,0 +1,85 @@
+﻿#include "Dialogue.h"
+#include "Engine.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Physics.h"
+#include "EntityManager.h"
+//El primer dialogo es el ultimo del vector!!
+Dialogue::Dialogue()
+{
+	
+	
+}
+
+Dialogue::~Dialogue() {
+	
+}
+
+bool Dialogue::Awake() {
+	return true;
+}
+
+bool Dialogue::Start() {
+
+	
+	
+	
+	
+
+
+	return true;
+}
+void Dialogue::Draw(float dt) {
+	if (hasStarted && lenght > 0) {
+		SDL_Texture* currentTexture = dialogue.back();
+		Engine::GetInstance().render->DrawTexture(currentTexture, xInicial, yInicial);
+	}
+}
+
+bool Dialogue::Update(float dt)
+{
+	if (!hasStarted) return true;
+
+	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
+	
+		NextDialogue();
+	
+	}
+	
+	return true;
+}
+void Dialogue::BeginDialogue() {
+
+	hasStarted = true;
+
+
+
+}
+void Dialogue:: NextDialogue() {
+	if (lenght > 0) {
+		dialogue.pop_back();
+		lenght--;
+	}
+	else {
+		hasStarted = false;
+	}
+}
+void Dialogue::AddDialogue(SDL_Texture* texture) {
+	dialogue.push_back(texture);
+	lenght++;
+}
+
+bool Dialogue::CleanUp()
+{
+	LOG("Unloading Coin");
+	for (int i = lenght; i < lenght; i++) {
+		dialogue.clear();
+
+	}
+	return true;
+}
+
---
+++ src/Dialogue.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Entity.h"
+#include "Animation.h"
+#include <box2d/box2d.h>
+#include <SDL3/SDL.h>
+#include "Pathfinding.h"
+
+struct SDL_Texture;
+
+class Dialogue 
+{
+public:
+	Dialogue();
+	virtual ~Dialogue();
+
+	bool Awake();
+	bool Start();
+	bool Update(float dt);
+	bool CleanUp();
+
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+	void NextDialogue();
+
+
+
+private:
+
+	void GetPhysicsValues();
+	
+	void ApplyPhysics();
+	void Draw(float dt);
+	bool hasStarted = false;
+
+
+private:
+	//Vector de texturas
+	std::vector<SDL_Texture*> dialogue;
+
+public:
+	
+	int xInicial;
+	int yInicial;	
+	int lenght = 0;
+	void AddDialogue(SDL_Texture* texture);
+	void BeginDialogue();
+
+	
+};
