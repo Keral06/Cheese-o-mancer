7271ee5e58db90f4ee76e818c5f0af169d04dd5d
LoreTheRat <burjons.bernat@gmail.com>
2026-04-23 00:37:44 +0200

    Decoració Torture Room, Main Dungeon
    Falta boss room (s'ha de fer amb el torture room tileset)

--- assets/Maps/tileset_LV1_dungeonBase_02.tsx
+++ assets/Maps/tileset_LV1_dungeonBase_02.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset_LV1_dungeonBase_02" tilewidth="128" tileheight="128" tilecount="810" columns="30">
- <image source="tileset_lv1_dungeonbase_03.png" width="3840" height="3537"/>
+<tileset version="1.10" tiledversion="1.12.0" name="tileset_LV1_dungeonBase_02" tilewidth="128" tileheight="128" tilecount="810" columns="30">
+ <image source="tileset_lv1_dungeon_base_04.png" width="3840" height="3537"/>
 </tileset>
