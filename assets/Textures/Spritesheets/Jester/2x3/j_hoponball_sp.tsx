9e63a5721e6f780e64b860fd74551e20fc28d67d
LluisCazorla <lluis.cazorla@estudiantat.upc.edu>
2026-04-22 12:55:26 +0200

    Added Jester Animations

---
+++ assets/Textures/Spritesheets/Jester/2x3/j_hoponball_sp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="j_hoponball_sp" tilewidth="256" tileheight="384" tilecount="14" columns="14">
+ <image source="j_hoponball.png" width="3584" height="384"/>
+</tileset>
--- assets/Textures/Spritesheets/Jester/2x3/j_sp.tsx
+++ assets/Textures/Spritesheets/Jester/2x3/j_sp.tsx
@@ -1,14 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="2x3" tilewidth="256" tileheight="384" tilecount="30" columns="10">
- <image source="j_2x3.png" width="2562" height="1156"/>
- <tile id="0">
+<tileset version="1.10" tiledversion="1.11.2" name="j_sp" tilewidth="256" tileheight="384" tilecount="42" columns="14">
+ <image source="../../../../../../../../Downloads/j_2x3.png" width="3584" height="1152"/>
+ <tile id="2">
   <properties>
-   <property name="loop" type="bool" value="true"/>
+   <property name="loop" type="bool" value="false"/>
    <property name="name" value="run"/>
   </properties>
   <animation>
-   <frame tileid="0" duration="100"/>
-   <frame tileid="1" duration="100"/>
    <frame tileid="2" duration="100"/>
    <frame tileid="3" duration="100"/>
    <frame tileid="4" duration="100"/>
@@ -17,41 +15,48 @@
    <frame tileid="7" duration="100"/>
    <frame tileid="8" duration="100"/>
    <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
   </animation>
  </tile>
- <tile id="10">
+ <tile id="16">
   <properties>
    <property name="loop" type="bool" value="false"/>
    <property name="name" value="jump"/>
   </properties>
   <animation>
-   <frame tileid="10" duration="100"/>
-   <frame tileid="11" duration="100"/>
-   <frame tileid="12" duration="100"/>
-   <frame tileid="13" duration="100"/>
-   <frame tileid="14" duration="100"/>
-   <frame tileid="15" duration="100"/>
    <frame tileid="16" duration="100"/>
    <frame tileid="17" duration="100"/>
    <frame tileid="18" duration="100"/>
    <frame tileid="19" duration="100"/>
-  </animation>
- </tile>
- <tile id="20">
-  <properties>
-   <property name="loop" type="bool" value="false"/>
-   <property name="name" value="hoponcheese"/>
-  </properties>
-  <animation>
    <frame tileid="20" duration="100"/>
    <frame tileid="21" duration="100"/>
    <frame tileid="22" duration="100"/>
    <frame tileid="23" duration="100"/>
    <frame tileid="24" duration="100"/>
    <frame tileid="25" duration="100"/>
-   <frame tileid="26" duration="100"/>
-   <frame tileid="27" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="hoponcheese"/>
+  </properties>
+  <animation>
    <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
   </animation>
  </tile>
 </tileset>
