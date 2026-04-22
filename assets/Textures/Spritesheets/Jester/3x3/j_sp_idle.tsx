959867c1a441874be8c9521d1817f34a9c52cd11
LluisCazorla <lluis.cazorla@estudiantat.upc.edu>
2026-04-22 13:15:08 +0200

    Added Jester Animations

---
+++ assets/Textures/Spritesheets/Jester/3x3/j_sp_idle.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="j_sp_idle" tilewidth="384" tileheight="384" tilecount="34" columns="17">
+ <image source="sprite_jester_idles_02.png" width="6528" height="768"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="idle"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="idleOnCheese"/>
+  </properties>
+  <animation>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
