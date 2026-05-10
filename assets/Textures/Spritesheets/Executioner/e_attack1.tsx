b46eaaa238aeb0f87a11f62170cf34c44c18d61d
Katy <sofiagiva1@gmail.com>
2026-05-10 21:41:14 +0200

    Cheese Executioner Animations .tsx

---
+++ assets/Textures/Spritesheets/Executioner/e_attack1.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="e_attack1" tilewidth="1664" tileheight="1024" tilecount="36" columns="6">
+ <image source="../../../../resources/spritesheets/Cheese Executoner/Nuevos en Grid/spritesheet ataque1.png" width="9984" height="6144"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="attack1"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/Textures/Spritesheets/Executioner/e_attack3_grab.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="e_attack3_grab" tilewidth="1920" tileheight="1792" tilecount="42" columns="6">
+ <image source="../../../../resources/spritesheets/Cheese Executoner/Nuevos en Grid/spritesheet ataque3 grab.png" width="11520" height="12544"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="attack3_grab"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="36" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
