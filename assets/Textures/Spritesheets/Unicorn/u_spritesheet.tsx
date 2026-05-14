72e13b59fc36b0ee6587cdea740bb6b53498345d
Katy <sofiagiva1@gmail.com>
2026-05-14 17:00:26 +0200

    Animations Knight Normal & Cheese, Lady Nohuely, Unicorn

---
+++ assets/Textures/Spritesheets/Unicorn/u_spritesheet.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="u_spritesheet" tilewidth="640" tileheight="640" tilecount="48" columns="12">
+ <image source="sprite_unicorn_b_02.png" width="7680" height="2560"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="idle"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="walk"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="charge"/>
+  </properties>
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="stop"/>
+  </properties>
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+   <property name="name" value="death"/>
+  </properties>
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
