6a2fd03aabaf4c0ac5f35a9a3f1f02b535dc1815
mecMocmec <e.ibanez3426@gmail.com>
2026-05-19 17:23:28 +0200

    maps update
    added lv2 and lv3 maps (some unfinished). New folders in Maps

---
+++ assets/Maps/PROV_tree big 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree big 2" tilewidth="128" tileheight="128" tilecount="17640" columns="140">
+ <image source="../../resources/Assets/nivell 2/Botanic/tree big 2.png" width="18000" height="16200"/>
+</tileset>
