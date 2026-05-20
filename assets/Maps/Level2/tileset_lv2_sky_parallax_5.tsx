f42183361da674bd79f33d3fb77859d96d6a1e44
LoreTheRat <128939423+LoreTheRat@users.noreply.github.com>
2026-05-20 14:33:49 +0200

    Intent de parallax cel i sala boss 2

---
+++ assets/Maps/Level2/tileset_lv2_sky_parallax_5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset_lv2_sky_parallax_5" tilewidth="7000" tileheight="2250" tilecount="1" columns="1">
+ <image source="Sky Parts/5.png" width="7000" height="2250"/>
+</tileset>
