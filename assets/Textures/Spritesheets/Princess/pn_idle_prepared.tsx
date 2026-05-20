bc6ae97594cb1bb95a9f7121dd5a17509a60c472
Katy <sofiagiva1@gmail.com>
2026-05-20 10:45:39 +0200

    princess idle .tsx created

---
+++ assets/Textures/Spritesheets/Princess/pn_idle_prepared.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pn_idle" tilewidth="384" tileheight="513" tilecount="36" columns="18">
+ <image source="princessN_Idle+Prepared.png" width="6914" height="1027"/>
+</tileset>
