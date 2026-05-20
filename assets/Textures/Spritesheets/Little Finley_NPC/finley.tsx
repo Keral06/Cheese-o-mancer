fbe4935b35c6cca56d2dfc51b0463dc021590e25
Katy <sofiagiva1@gmail.com>
2026-05-20 12:17:32 +0200

    .tsx Sculptor, Finley, Milkmaid, Monk

---
+++ assets/Textures/Spritesheets/Little Finley_NPC/finley.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="finley" tilewidth="256" tileheight="385" tilecount="12" columns="4">
+ <image source="Finley_spritesheet.png" width="1029" height="1156"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="idle"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
