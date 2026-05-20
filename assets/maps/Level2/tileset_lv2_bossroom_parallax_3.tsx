63a69a80defc8c696a149b4312f31bb746ba9a2c
LoreTheRat <burjons.bernat@gmail.com>
2026-05-20 19:37:01 +0200

    Layouts nivell 2 update

---
+++ assets/maps/Level2/tileset_lv2_bossroom_parallax_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="tileset_lv2_bossroom_parallax_3" tilewidth="6528" tileheight="2560" tilecount="1" columns="1">
+ <image source="Boss Room/niv2_ilust_bossfight_foreground.png" width="6528" height="2560"/>
+</tileset>
---
+++ assets/maps/Level2/tileset_lv2_botanic_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="tileset_lv2_botanic_01" tilewidth="128" tileheight="128" tilecount="1368" columns="38">
+ <image source="tileset_lv2_botanic_01.png" width="4864" height="4608"/>
+</tileset>
