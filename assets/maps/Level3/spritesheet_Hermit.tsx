a1d1f2056ff2c2cd7b499afe0979ebda7ff40847
LoreTheRat <burjons.bernat@gmail.com>
2026-05-20 23:46:18 +0200

    Hermit tsx fix

--- assets/maps/Level3/spritesheet_Hermit.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="spritesheet_Hermit" tilewidth="512" tileheight="512" tilecount="60" columns="15">
- <image source="../../Textures/Spritesheets/Hermit/spritesheet_Hermit.png" width="7680" height="2048"/>
-</tileset>
