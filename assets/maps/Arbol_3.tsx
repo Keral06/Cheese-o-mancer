dde308e046bcb88db20c877610ce9a801a42cd8b
LoreTheRat <burjons.bernat@gmail.com>
2026-05-21 14:26:56 +0200

    Canvis ultima hora layout

---
+++ assets/maps/Arbol_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Arbol_3" tilewidth="9800" tileheight="7202" tilecount="1" columns="1">
+ <image source="Arbol_3.png" width="9800" height="7202"/>
+</tileset>
