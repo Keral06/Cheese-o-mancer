628b28d13683cda6be69e1f58bd832a8e7ba1ff8
mecMocmec <e.ibanez3426@gmail.com>
2026-05-26 13:54:19 +0200

    urgent map fixes

--- assets/maps/Arbol_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="Arbol_2" tilewidth="9800" tileheight="7202" tilecount="1" columns="1">
- <image source="Arbol_2.png" width="9800" height="7202"/>
-</tileset>
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -774,7 +774,7 @@ void Scene::UpdateLevel(float dt) {
 		LoadMap("Map_LV2_bossTower.tmx");
 	}
 	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_P) == KEY_DOWN) {
-		LoadMap("Map_LV3_left_01.tmx");
+		LoadMap("Level2/Map_LV2_botanica_01.tmx");
 	}
 	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_O) == KEY_DOWN) {
 		LoadMap("Map_LV2_encreuada_02.tmx");
@@ -783,7 +783,7 @@ void Scene::UpdateLevel(float dt) {
 		LoadMap("Map_LV3_right_02.tmx");
 	}
 	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_C) == KEY_DOWN) {
-		LoadMap("Map_LV2_towersBotanica_02.tmx");
+		LoadMap("Map_LV2_botanica_02.tmx");
 	}
 
 
