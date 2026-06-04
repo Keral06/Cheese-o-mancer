13bdc7866f9f922f40480311dad680d88fbfa6a7
Katy <sofiagiva1@gmail.com>
2026-06-04 19:13:44 +0200

    Animations  .tsx

---
+++ assets/Textures/Spritesheets/Knight/Cheese/kc_idle.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="kc_idle" tilewidth="640" tileheight="768" tilecount="12" columns="4">
+ <image source="KnightC_Idle.png" width="2560" height="2304"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="idle"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
