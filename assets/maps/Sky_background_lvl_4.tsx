eeddd0648c7774920fd49aa362a7af618bf19949
LoreTheRat <burjons.bernat@gmail.com>
2026-06-06 23:47:44 +0200

    Parallax fix

---
+++ assets/maps/Sky_background_lvl_4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Sky_background_lvl_4" tilewidth="7000" tileheight="2250" tilecount="1" columns="1">
+ <image source="sky lvl4/Sky_background_lvl_4.png" width="7000" height="2250"/>
+</tileset>
---
+++ assets/maps/cloud_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="cloud_1" tilewidth="7000" tileheight="2250" tilecount="1" columns="1">
+ <image source="sky lvl4/cloud_1.png" width="7000" height="2250"/>
+</tileset>
