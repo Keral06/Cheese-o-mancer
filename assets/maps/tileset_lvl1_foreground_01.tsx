834bcd340801ab6d0c3fa517781df0fbb94507cb
LoreTheRat <burjons.bernat@gmail.com>
2026-06-07 01:02:19 +0200

    Lvl1 integration revision

---
+++ assets/maps/tileset_lvl1_foreground_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="tileset_lvl1_foreground_01" tilewidth="128" tileheight="128" tilecount="1320" columns="44">
+ <image source="tileset_lvl1_foreground_01.png" width="5632" height="3840"/>
+</tileset>
