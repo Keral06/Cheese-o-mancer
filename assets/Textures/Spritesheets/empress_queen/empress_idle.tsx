4c1af545f22f5a62a0abe3dc2dcfb690fa11887f
Katy <sofiagiva1@gmail.com>
2026-06-07 23:19:05 +0200

    Animation for Empress

---
+++ assets/Textures/Spritesheets/empress_queen/empress_idle.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="empress_idle" tilewidth="512" tileheight="512" tilecount="16" columns="16">
+ <image source="empress_01_idle.png" width="8192" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="loop" type="bool" value="true"/>
+   <property name="name" value="idle"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
