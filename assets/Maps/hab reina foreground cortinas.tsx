ab23dd894d4bb1bbcdb426bfc1f1c07707c9ad5a
LoreTheRat <128939423+LoreTheRat@users.noreply.github.com>
2026-06-08 19:29:30 +0200

    Tileset lvl 4

---
+++ assets/Maps/hab reina foreground cortinas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="hab reina foreground cortinas" tilewidth="4096" tileheight="2048" tilecount="1" columns="1">
+ <image source="cuarto reina/hab reina foreground cortinas.png" width="4096" height="2048"/>
+</tileset>
---
+++ assets/Maps/habitacion reina.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="habitacion reina" tilewidth="4096" tileheight="2048" tilecount="1" columns="1">
+ <image source="cuarto reina/habitacion reina.png" width="4096" height="2048"/>
+</tileset>
