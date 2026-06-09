313d447614ec40f5b9d81dba1f1521b7f6bb8bbd
Irene <icalatravasierra@gmail.com>
2026-06-09 20:20:34 +0200

    put all tarots

--- src/NPC.cpp
+++ src/NPC.cpp
@@ -3141,6 +3141,7 @@ const SDL_Rect& animFrame = anims.GetCurrentFrame();
 					if (level1.AvanzarDialogo(dt)) {
 
 						Engine::GetInstance().scene->hasTalkedAboutPsalmsB4 = true;
+						Engine::GetInstance().scene->misiones.Completed("Psalms");
 					}
 					return true;
 				
@@ -3180,6 +3181,8 @@ const SDL_Rect& animFrame = anims.GetCurrentFrame();
 				if (level2.AvanzarDialogo(dt)) {
 
 					Engine::GetInstance().scene->hasBeenWhistledblowed = true;
+					Engine::GetInstance().scene->misiones.push("KillPriest", Engine::GetInstance().textures->Load("assets/UI/UI_Mission_Info/UI_MissionNotes_Hierophant1_02.png"), Engine::GetInstance().textures->Load("assets/UI/UI_Mission_Info/UI_MissionNotes_Hierophant2_02.png"));
+
 				}
 				return true;
 			
@@ -3218,7 +3221,7 @@ const SDL_Rect& animFrame = anims.GetCurrentFrame();
 
 				if (hasAll.AvanzarDialogo(dt)) {
 
-					SDL_Texture* help = Engine::GetInstance().textures->Load("assets/UI/UI_Mission_Items/UI_Mission_ItemGargantualTreeRoot1_.png");
+					SDL_Texture* help = Engine::GetInstance().textures->Load("assets/UI/UI_Mission_Items/UI_Mission_ItemQueenKey1.png");
 					Engine::GetInstance().scene->inventario.push("EmpressKey1", help, nullptr);
 					Engine::GetInstance().scene->misiones.Completed("Psalms");
 					Engine::GetInstance().scene->cards.push("Hierophant", Engine::GetInstance().textures->Load("assets/UI/Tarot/UI_TarotCard_Hierophant.png"), nullptr);
@@ -4412,7 +4415,7 @@ const SDL_Rect& animFrame = anims.GetCurrentFrame();
 
 						Engine::GetInstance().scene->misiones.Completed("Retired");
 
-
+						Engine::GetInstance().scene->cards.push("Strenght", Engine::GetInstance().textures->Load("assets/UI/Tarot/UI_TarotCard_Strength.png"), nullptr);
 					}
 					
 
