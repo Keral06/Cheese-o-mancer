635f41535b987f1f299fa7a6247e807396d3fa11
LoreTheRat <burjons.bernat@gmail.com>
2026-06-09 20:56:43 +0200

    General Map Fixes

---
+++ assets/maps/tileset_lv1_Pantry_05.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="tileset_lv1_Pantry_05" tilewidth="128" tileheight="128" tilecount="810" columns="30">
+ <image source="tileset_lv1_Pantry_05.png" width="3840" height="3537"/>
+</tileset>
