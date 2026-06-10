bd3c30ebe81af1fce8ec13c83c5e86d2fef25a25
LoreTheRat <burjons.bernat@gmail.com>
2026-06-10 02:49:00 +0200

    muralla fix justas

---
+++ assets/maps/muralla_torre_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="muralla_torre" tilewidth="128" tileheight="128" tilecount="1242" columns="46">
+ <image source="muralla_torre.png" width="6000" height="3456"/>
+</tileset>
